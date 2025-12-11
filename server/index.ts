f6e05b5d9778b4a74e5d0bba51116bc9da167c26
Wes Imac mini 1M <wes@wess-mac-mini.myfiosgateway.com>
2025-12-11 18:11:05 -0500

    Cleanup: Remove auto-migration script as it was run manually

--- server/index.ts
+++ server/index.ts
@@ -17,11 +17,8 @@ import attomRoutes from './routes/attomRoutes.js';
 // Load environment variables
 dotenv.config();
 
-import { runAutoMigration } from './utils/automigrate.js';
-
 // --- STARTUP DIAGNOSTICS ---
 console.log('--- SERVER STARTUP ---');
-await runAutoMigration(); // Ensure DB schema is up to date
 
 const clientId = process.env.VITE_GOOGLE_CLIENT_ID;
 if (clientId && clientId !== 'undefined' && !clientId.includes('YOUR_GOOGLE_CLIENT_ID_HERE')) {
